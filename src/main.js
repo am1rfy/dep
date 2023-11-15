fd06dd7c3b5b8666f98e9aab5a15025c963555e0
am1rfy <fajzov.amir.gl@gmail.com>
2023-11-15 15:50:22 +0500

    added element plus

--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
+import ElementPlus, { dayjs } from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import ru from 'element-plus/es/locale/lang/ru'
+import 'element-plus/dist/index.css'
+import 'dayjs/locale/ru'
+
+dayjs.locale('ru')
+
 const app = createApp(App)
 
+for (const [key, component] of Object.entries(ElementPlusIconsVue))
+  app.component(key, component)
+
 app
+  .use(ElementPlus, { locale: ru })
   .use(router)
   .mount('#app')
